e93c16404c72ad1ea3ac730eb06a51c233635505
YearsLaterFixed <fabian.suhr1@gmx.de>
2026-09-07 19:23:55 +0200

    correct LF

--- tests/auto/qamqpparser/qamqpparser.pro
+++ tests/auto/qamqpparser/qamqpparser.pro
@@ -3,9 +3,4 @@ include($${DEPTH}/qamqp.pri)
 include($${DEPTH}/tests/tests.pri)
 
 TARGET = tst_qamqpparser
-SOURCES = tst_qamqpparser.cppDEPTH = ../../..
-include($${DEPTH}/qamqp.pri)
-include($${DEPTH}/tests/tests.pri)
-
-TARGET = tst_qamqpparser
 SOURCES = tst_qamqpparser.cpp
--- tests/auto/qamqpparser/tst_qamqpparser.cpp
+++ tests/auto/qamqpparser/tst_qamqpparser.cpp
@@ -7,6 +7,7 @@ class tst_QAMQPParser : public QObject
     Q_OBJECT
 private Q_SLOTS:
     void rejectsExcessivelyNestedArray();
+    void rejectsExcessivelyNestedTable();
 };
 
 void tst_QAMQPParser::rejectsExcessivelyNestedArray()
@@ -32,6 +33,35 @@ void tst_QAMQPParser::rejectsExcessivelyNestedArray()
     QVariant value = QAmqpTable::readFieldValue(input, QAmqpMetaType::Array);
 
     QVERIFY(!value.isValid());
+    QCOMPARE(input.status(), QDataStream::ReadCorruptData);
+}
+
+void tst_QAMQPParser::rejectsExcessivelyNestedTable()
+{
+    const int depth = 33;
+    QByteArray nestedTable;
+    QDataStream emptyTable(&nestedTable, QIODevice::WriteOnly);
+    emptyTable << quint32(0);
+
+    for (int i = 1; i < depth; ++i) {
+        QByteArray tableBody;
+        QDataStream tableBodyStream(&tableBody, QIODevice::WriteOnly);
+        tableBodyStream << quint8(1);
+        tableBodyStream.writeRawData("n", 1);
+        tableBodyStream << qint8('F');
+        tableBodyStream.writeRawData(nestedTable.constData(), nestedTable.size());
+
+        QByteArray wrappedTable;
+        QDataStream wrappedStream(&wrappedTable, QIODevice::WriteOnly);
+        wrappedStream << quint32(tableBody.size());
+        wrappedStream.writeRawData(tableBody.constData(), tableBody.size());
+        nestedTable = wrappedTable;
+    }
+
+    QDataStream input(&nestedTable, QIODevice::ReadOnly);
+    QAmqpTable table;
+    input >> table;
+
     QCOMPARE(input.status(), QDataStream::ReadCorruptData);
 }
 
